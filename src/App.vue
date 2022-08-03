952bef27ade706b2235c6c8aeda3c9bc8e6c6e20
MiracleHope <1102778734@qq.com>
2022-08-03 21:05:17 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+
+</style>
